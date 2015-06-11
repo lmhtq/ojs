35dfc88c7d5abd2b56d519b13ae3ea74c63c6a67
lmhtq <lmhtq1991@gmail.com>
2015-06-11 22:52:11 +0800

    hiho46, undone

--- hihocoder/hiho46/sprague_grundy_nim3.cpp
+++ hihocoder/hiho46/sprague_grundy_nim3.cpp
@@ -1,8 +1,33 @@
 #include <cstdio>
+#include <cstring>
 #include <queue>
 #include <cstring>
 
+#define N 20
+int sg[N], hash[N];
+
+void Calc_SG()
+{
+	memset(hash, 0, sizeof(N));
+	sg[0] = 0;
+	sg[1] = 1;
+	for (int i = 2; i < N; i++) {
+		
+	}
+}
+
 int main()
 {
+	int sg[13];
+	sg[0] = 0;
+	sg[1] = 1;
+	for (int j = 1; j < 13; j++) {
+		vector<int> a;
+		a.push_back(sg[0]);
+		for (int i = 1; i < j/2; i++) {
+
+		}
+
+	}
 	return 0;
 }
